18e73d3be63a27f6efaf5842d5432e4c8c978bf9
JosueDLA <josuedlavs@gmail.com>
2020-10-13 11:55:02 -0600

    Created Image Class

--- cv_tools/image.py
+++ cv_tools/image.py
@@ -2,6 +2,14 @@ import numpy as np
 import cv2
 
 
-def select_image(path, height, width):
-    image = cv2.imread(path)
-    image = cv2.resize(image, (width, height))
+class Image:
+    def __init__(self, path, height, width):
+        self.path = path
+        self.height = height
+        self.width = width
+
+    def select(self):
+        image = cv2.imread(self.path)
+        image = cv2.resize(image, (self.width, self.height))
+
+        return image
