55f21f12c84c8fbb98dbf9ddef4f536a41aeae19
JosueDLA <josuedlavs@gmail.com>
2020-10-14 08:00:23 -0600

    Created pipfile and requirements.txt

--- cv_tools/video.py
+++ cv_tools/video.py
@@ -16,7 +16,6 @@ class Video:
 
     def read(self, video_capture):
         ret, frame = video_capture.read()
-
         frame = Frame(frame, self.height, self.width)
 
         return ret, frame
@@ -24,7 +23,6 @@ class Video:
     def write(self, output_path):
         #cv2.VideoWriter(name, codec, fps, resolution)
         fourcc = cv2.VideoWriter_fourcc(*'MPEG')
-
         out = cv2.VideoWriter(output_path, fourcc, 30,
                               (self.width, self.height))
 
